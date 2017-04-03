5c44ee07226107a8209cc1b9f052b1a446506a60
GadusBetter <583854771@qq.com>
2017-04-03 23:03:35 +0800

    .

---
+++ ygq/app/src/main/java/com/wb/ygq/bean/HomeVideoBean.java
@@ -0,0 +1,126 @@
+package com.wb.ygq.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/3.
+ */
+public class HomeVideoBean {
+
+    /**
+     * Msg : 200
+     * Count : 2
+     * Data : {"carouselList":[{"img":"http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg","url":"http://www.baidu.com"}],"indexImgList":[{"img":"","url":"","title":"限时免费"}]}
+     */
+
+    private String Msg;
+    private String Count;
+    private DataBean Data;
+
+    public String getMsg() {
+        return Msg;
+    }
+
+    public void setMsg(String Msg) {
+        this.Msg = Msg;
+    }
+
+    public String getCount() {
+        return Count;
+    }
+
+    public void setCount(String Count) {
+        this.Count = Count;
+    }
+
+    public DataBean getData() {
+        return Data;
+    }
+
+    public void setData(DataBean Data) {
+        this.Data = Data;
+    }
+
+    public static class DataBean {
+        /**
+         * img : http://pic6.huitu.com/res/20130116/84481_20130116142820494200_1.jpg
+         * url : http://www.baidu.com
+         */
+
+        private List<CarouselListBean> carouselList;
+        /**
+         * img :
+         * url :
+         * title : 限时免费
+         */
+
+        private List<IndexImgListBean> indexImgList;
+
+        public List<CarouselListBean> getCarouselList() {
+            return carouselList;
+        }
+
+        public void setCarouselList(List<CarouselListBean> carouselList) {
+            this.carouselList = carouselList;
+        }
+
+        public List<IndexImgListBean> getIndexImgList() {
+            return indexImgList;
+        }
+
+        public void setIndexImgList(List<IndexImgListBean> indexImgList) {
+            this.indexImgList = indexImgList;
+        }
+
+        public static class CarouselListBean {
+            private String img;
+            private String url;
+
+            public String getImg() {
+                return img;
+            }
+
+            public void setImg(String img) {
+                this.img = img;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+        }
+
+        public static class IndexImgListBean {
+            private String img;
+            private String url;
+            private String title;
+
+            public String getImg() {
+                return img;
+            }
+
+            public void setImg(String img) {
+                this.img = img;
+            }
+
+            public String getUrl() {
+                return url;
+            }
+
+            public void setUrl(String url) {
+                this.url = url;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+        }
+    }
+}
---
+++ ygq/app/src/main/java/com/wb/ygq/utils/HttpUrl.java
@@ -0,0 +1,14 @@
+package com.wb.ygq.utils;
+
+/**
+ * Created by Administrator on 2017/4/3.
+ */
+public class HttpUrl {
+    public static class API{
+        /**
+         * 首页
+         */
+        public static final String SHOUYE="http://youguo.fzjydqg.com/index.php/Api/Index/index";
+
+    }
+}
