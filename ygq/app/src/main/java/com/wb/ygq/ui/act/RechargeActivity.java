e5838e4e11080b2798946a05e2fed3632483cd64
guowenzeng <gwz@tangys.net>
2017-04-04 23:38:22 +0800

    充值act

---
+++ ygq/app/src/main/java/com/wb/ygq/ui/act/RechargeActivity.java
@@ -0,0 +1,56 @@
+package com.wb.ygq.ui.act;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.wb.ygq.R;
+import com.wb.ygq.ui.base.BaseActivity;
+
+/**
+ * Description：充值
+ * Created on 2017/4/4
+ * Author :
+ */
+public class RechargeActivity extends BaseActivity {
+
+
+    private Toolbar toolbar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_recharge);
+        initView();
+        initData();
+        setListener();
+    }
+
+    @Override
+    public void initTitle() {
+
+    }
+
+    @Override
+    public void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar_rechange);
+        toolbar.setNavigationIcon(R.drawable.back_black);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+}
