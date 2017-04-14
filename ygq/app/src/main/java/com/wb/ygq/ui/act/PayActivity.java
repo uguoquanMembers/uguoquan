e5ae000b416a96ca5d6ae08c78593b58b0a3692f
guowenzeng <gwz@tangys.net>
2017-04-15 01:32:21 +0800

    。

---
+++ ygq/app/src/main/java/com/wb/ygq/ui/act/PayActivity.java
@@ -0,0 +1,37 @@
+package com.wb.ygq.ui.act;
+
+import android.os.Bundle;
+
+import com.wb.ygq.ui.base.BaseActivity;
+
+/**
+ * Description：
+ * Created on 2017/4/15
+ */
+public class PayActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initTitle() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+}
